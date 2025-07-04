a4aa770f9a26371d015e13f5803804a49ccd8195
NazarethR <nazareth2006ramirez@gmail.com>
2025-07-04 15:46:17 -0600

    Se creo la clase Equipo
    Se creo la clase Equipo con sus metodos get,set, constructor y toString, falta el estado

---
+++ src/Equipos/Equipo.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Equipos;
+
+/**
+ *
+ * @author Student
+ */
+public class Equipo {
+    public String Codigo;
+    public String Descripcion;
+    public String Ubicacion;
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public Equipo(String Codigo, String Descripcion, String Ubicacion) {
+        this.Codigo = Codigo;
+        this.Descripcion = Descripcion;
+        this.Ubicacion = Ubicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" + "Codigo=" + Codigo + ", Descripcion=" + Descripcion + ", Ubicacion=" + Ubicacion + '}';
+    }
+
+   
+  
+    
+    
+    
+    
+    
+    
+    
+    
+}
